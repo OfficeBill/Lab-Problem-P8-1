e12dbb1d4d687b95e5770d8e2381e371fb62f1ff
OfficeBill <w-inkrott@onu.edu>
2024-10-31 09:08:08 -0400

    Completed countCharacter function

---
+++ Lab Problem P8-1/P8-1.cpp
@@ -0,0 +1,25 @@
+// File Name:     Lab Problem 8.1
+// Author:        William Inkrott
+// Date:          10/31/24
+// Description:   Determine the number of vowels in an inputted character. 
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int countCharacter(string str);
+
+int main(void)
+{
+	
+}
+
+int countCharacter(string input)
+{
+	int numofcharacters;
+	numofcharacters = str.length();
+	return numofcharacters;
+}
